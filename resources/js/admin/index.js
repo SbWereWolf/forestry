d3dfcdbc59c4efcff291c80bb54deffde120ba8b
FORGE-JITA\ktokt <ulfnew@gmail.com>
2020-05-20 19:04:20 +0500

    Fix redundant import at /resources/js/admin/index.js

--- resources/js/admin/index.js
+++ resources/js/admin/index.js
@@ -2,8 +2,6 @@ import './admin-user';
 import './profile-edit-profile';
 import './profile-edit-password';
 import './wood-specie';
-import './bonitet';
-import './timber-class';
 import './forest-resource';
 import './forestry-indicator';
 import './cutting-area';
